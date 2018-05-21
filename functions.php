189209eea654f72820ae5d098978384adee93395
Forrest Wilson <forrest.wilson@me.com>
2018-05-21 22:35:13 +1200

    Enqueuing custom JS and CSS along with including the bootstrap walker

--- functions.php
+++ functions.php
@@ -129,6 +129,15 @@ function humane_society_scripts() {
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
 	}
+
+	// Custom CSS Enqueueing
+	wp_enqueue_style( 'bootstrap', get_template_directory_uri() . '/node_modules/bootstrap/dist/css/bootstrap.min.css', array(), '4.1.1' );
+	wp_enqueue_style( 'custom-css', get_template_directory_uri() . '/assets/stylesheets/css/style.css', array(), '1.0.0' );
+
+	// Custom JS Enqueueing
+	wp_enqueue_script( 'jq', get_template_directory_uri() . '/node_modules/jquery/dist/jquery.min.js', array(), '3.3.1' );
+	wp_enqueue_script( 'bootstrap', get_template_directory_uri() . '/node_modules/bootstrap/dist/js/bootstrap.min.js', array(), '4.1.1' );
+	wp_enqueue_script( 'custom-js', get_template_directory_uri() . '/assets/scripts/app.min.js', array(), '1.0.0' );
 }
 add_action( 'wp_enqueue_scripts', 'humane_society_scripts' );
 
@@ -159,3 +168,5 @@ if ( defined( 'JETPACK__VERSION' ) ) {
 	require get_template_directory() . '/inc/jetpack.php';
 }
 
+// Register Custom Navigation Walker
+require_once get_template_directory() . '/class-wp-bootstrap-navwalker.php';
