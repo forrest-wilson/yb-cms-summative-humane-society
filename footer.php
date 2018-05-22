0624b4685be1d534667ff64014c484ffb013965f
Forrest Wilson <forrest.wilson@me.com>
2018-05-22 21:14:40 +1200

    Wrapping footer in a container

--- footer.php
+++ footer.php
@@ -15,17 +15,19 @@
 
 	<footer id="colophon" class="site-footer">
 		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'humane-society' ) ); ?>">
-				<?php
-				/* translators: %s: CMS name, i.e. WordPress. */
-				printf( esc_html__( 'Proudly powered by %s', 'humane-society' ), 'WordPress' );
-				?>
-			</a>
-			<span class="sep"> | </span>
-				<?php
-				/* translators: 1: Theme name, 2: Theme author. */
-				printf( esc_html__( 'Theme: %1$s by %2$s.', 'humane-society' ), 'humane-society', '<a href="https://forrest-wilson.com">Forrest Wilson-Jennings</a>' );
-				?>
+			<div class="container">
+				<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'humane-society' ) ); ?>" target="_blank">
+					<?php
+					/* translators: %s: CMS name, i.e. WordPress. */
+					printf( esc_html__( 'Proudly powered by %s', 'humane-society' ), 'WordPress' );
+					?>
+				</a>
+				<span class="sep"> | </span>
+					<?php
+					/* translators: 1: Theme name, 2: Theme author. */
+					printf( esc_html__( 'Theme: %1$s by %2$s.', 'humane-society' ), 'humane-society', '<a href="https://forrest-wilson.com" target="_blank">Forrest Wilson-Jennings</a>' );
+					?>
+			</div>
 		</div><!-- .site-info -->
 	</footer><!-- #colophon -->
 </div><!-- #page -->
