b9dbac9edb4137a6405f76f05015a50a52857376
Forrest Wilson <forrest.wilson@me.com>
2018-05-25 13:50:27 +1200

    Basic layout styling

--- single.php
+++ single.php
@@ -12,28 +12,36 @@ get_header();
 
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main">
+			<div class="container">
+				<div class="row">
+					<div class="col-sm-12 col-md-8 col-lg-9">
+					<?php
+						while ( have_posts() ) :
+							the_post();
 
-		<?php
-		while ( have_posts() ) :
-			the_post();
+							echo get_post_type();
 
-			echo get_post_type();
+							get_template_part( 'template-parts/content', get_post_type() );
 
-			get_template_part( 'template-parts/content', get_post_type() );
+							the_post_navigation();
 
-			the_post_navigation();
+							// If comments are open or we have at least one comment, load up the comment template.
+							if ( comments_open() || get_comments_number() ) :
+								comments_template();
+							endif;
 
-			// If comments are open or we have at least one comment, load up the comment template.
-			if ( comments_open() || get_comments_number() ) :
-				comments_template();
-			endif;
+						endwhile; // End of the loop.
+					?>
+					</div>
 
-		endwhile; // End of the loop.
-		?>
+					<div class="col-sm-12 col-md-4 col-lg-3">
+					<?php get_sidebar(); ?>
+					</div>
+				</div>
+			</div>
 
 		</main><!-- #main -->
 	</div><!-- #primary -->
 
 <?php
-get_sidebar();
 get_footer();
