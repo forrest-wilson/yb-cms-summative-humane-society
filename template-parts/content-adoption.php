c0befaf0771754b1662b613b79eb445f5b22afa2
Forrest Wilson <forrest.wilson@me.com>
2018-05-28 16:19:37 +1200

    Adding "Edit" button to pages when logged in

--- template-parts/content-adoption.php
+++ template-parts/content-adoption.php
@@ -46,4 +46,26 @@
     </div>
     <hr>
     <div><?php the_content(); ?></div>
+    <?php if ( get_edit_post_link() ) : ?>
+		<footer class="entry-footer">
+			<?php
+			edit_post_link(
+				sprintf(
+					wp_kses(
+						/* translators: %s: Name of current post. Only visible to screen readers */
+						__( 'Edit <span class="screen-reader-text">%s</span>', 'humane-society' ),
+						array(
+							'span' => array(
+								'class' => array(),
+							),
+						)
+					),
+					get_the_title()
+				),
+				'<span class="edit-link">',
+				'</span>'
+			);
+			?>
+		</footer><!-- .entry-footer -->
+	<?php endif; ?>
 </div>
