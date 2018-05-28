477da0f1c62b015d75708ddd622652746a952454
Forrest Wilson <forrest.wilson@me.com>
2018-05-28 16:53:29 +1200

    Adding alt tag to header logo

--- template-parts/header-nav.php
+++ template-parts/header-nav.php
@@ -12,8 +12,8 @@
 <nav class="navbar navbar-expand-lg navbar-dark" style="background-color: rgb(1, 66, 130);">
     <div class="container">
         <a href="#" class="navbar-brand">
-            <img src="<?php bloginfo("stylesheet_directory"); ?>/assets/img/cat-and-dog.gif" class="animal-sketch" height="100" width="100" />
-            <img src="<?php bloginfo("stylesheet_directory"); ?>/assets/img/humane-society.gif" class="name" style="max-width: 300px" />
+            <img src="<?php bloginfo("stylesheet_directory"); ?>/assets/img/cat-and-dog.gif" alt="Humane Society Logo Part" class="animal-sketch" height="100" width="100" />
+            <img src="<?php bloginfo("stylesheet_directory"); ?>/assets/img/humane-society.gif" alt="Humane Society Logo Part" class="name" style="max-width: 300px" />
         </a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
