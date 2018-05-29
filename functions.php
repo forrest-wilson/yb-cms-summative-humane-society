c0cb1fea12fe000501f1a48445cb5e44954b4bc5
Forrest Wilson <forrest.wilson@me.com>
2018-05-29 16:26:10 +1200

    Unregistering RSS and Meta widget

--- functions.php
+++ functions.php
@@ -210,6 +210,8 @@ function unregister_widgets() {
 	unregister_widget('WP_Widget_Calendar');
 	unregister_widget('WP_Widget_Recent_Comments');
 	unregister_widget('WP_Widget_Recent_Posts');
+	unregister_widget('WP_Widget_RSS');
+	unregister_widget('WP_Widget_Meta');
 }
 
 add_action('widgets_init', 'unregister_widgets');
